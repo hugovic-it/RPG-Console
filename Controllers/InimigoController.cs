1a3b55f62a26528f4d05fb5858a77b9e92cee2a4
Hugo Victor <hugovic.it@gmail.com>
2023-04-29 09:46:09 -0300

    inimigoController e menu do program atualizado

--- Controllers/InimigoController.cs
+++ Controllers/InimigoController.cs
@@ -2,11 +2,124 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using rpg_console.Context;
+using rpg_console.Models;
 
 namespace rpg_console.Controllers
 {
     public class InimigoController
     {
+        private readonly AppDbContext _context;
+
         
+        public InimigoController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+         public void CadastrarInimigo(){
+            try{
+                System.Console.WriteLine("Inserir dados do Inimigo: \n");
+                System.Console.Write("Inserir o Nome: ");
+                string nome = Console.ReadLine();
+                System.Console.Write("Inserir a quantidade de HP: ");
+                int hp = int.Parse(Console.ReadLine());
+                System.Console.Write("Inserir a quantidade de MP: ");
+                int mp = int.Parse(Console.ReadLine());
+                System.Console.Write("Inserir a quantidade de Forca: ");
+                int forca = int.Parse(Console.ReadLine());
+                System.Console.Write("Inserir a quantidade de Defesa: ");
+                int defesa = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("Inserir a quantidade de Experiencia: ");
+                int experiencia = int.Parse(Console.ReadLine());
+                
+
+                Inimigo inimigo = new Inimigo{
+                    Nome = nome,
+                    Hp = hp,
+                    Mp = mp,
+                    Forca = forca,
+                    Defesa = defesa,
+                    Experiencia = experiencia
+                };
+
+                _context.Add(inimigo);
+                _context.SaveChanges();
+            }
+            catch(Exception e){
+                System.Console.WriteLine("Ocorreu um erro: " + e.Message);
+            }
+            System.Console.WriteLine("Inimigo Cadastrado com Sucesso!");
+    }
+
+        public void VerInimigo(){
+            System.Console.WriteLine("Informações do inimigo: ");
+            System.Console.WriteLine(_context.Inimigos.FirstOrDefault(x => x.InimigoId == 1));
+        }
+        public void VerTodosInimigos(){
+            System.Console.WriteLine("Informações dos Inimigos: ");
+            var inimigos = _context.Inimigos.ToList();
+
+            foreach(Inimigo inimigo in inimigos ){
+                System.Console.WriteLine(inimigo.ToString());
+            }
+        }
+
+        public void ModificarInimigoEspecifico(){
+            System.Console.WriteLine("Modificar Inimigo Especifico");
+            System.Console.Write("Inserir ID do inimigo: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Inimigo inimigo = _context.Inimigos.FirstOrDefault(x => x.InimigoId == id);
+           
+            if(inimigo == null){
+                System.Console.WriteLine("Jogador não encontrado!");
+                return;
+            }
+            
+            System.Console.WriteLine("Informações atuais do inimigo: ");
+            System.Console.WriteLine(inimigo);
+            System.Console.WriteLine("Inserir dados do inimigo: ");
+            System.Console.Write("Inserir o Nome: ");
+            string nome = Console.ReadLine();
+            System.Console.Write("Inserir a quantidade de HP: ");
+            int hp = int.Parse(Console.ReadLine());
+            System.Console.Write("Inserir a quantidade de MP: ");
+            int mp = int.Parse(Console.ReadLine());
+            System.Console.Write("Inserir a quantidade de Forca: ");
+            int forca = int.Parse(Console.ReadLine());
+            System.Console.Write("Inserir a quantidade de Defesa: ");
+            int defesa = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Inserir a quantidade de Experiencia: ");
+            int experiencia = int.Parse(Console.ReadLine());
+
+                inimigo.InimigoId = id;
+                inimigo.Nome = nome;
+                inimigo.Hp = hp;
+                inimigo.Mp = mp;
+                inimigo.Forca = forca;
+                inimigo.Defesa = defesa;
+            
+            _context.Inimigos.Update(inimigo);
+            _context.SaveChanges();
+
+            System.Console.WriteLine("Inimigo modificado!");
+        }
+        public void RemoverInimigoEspecifico(){
+            System.Console.WriteLine("Remover Inimigo Especifico");
+            System.Console.Write("Inserir ID do inimigo: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Inimigo inimigo = _context.Inimigos.FirstOrDefault(x => x.InimigoId == id);
+           
+            if(inimigo == null){
+                System.Console.WriteLine("Inimigo não encontrado!");
+                return;
+            }
+
+            _context.Inimigos.Remove(inimigo);
+            _context.SaveChanges();
+            return;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,41 +11,81 @@ class Program
     {
         AppDbContext contexto = new AppDbContext();
         JogadorController jogadorController = new JogadorController(contexto);
+        InimigoController inimigoController = new InimigoController(contexto);
 
         Console.WriteLine("Olár!");
 
         int menu = 0;
-        while(menu != 6){
-
+        int submenu = 0;
+        while(menu != 3){
             System.Console.WriteLine(" ============= Menu ============= ");
-            System.Console.WriteLine(" 1 -> Cadastrar Jogador ");
-            System.Console.WriteLine(" 2 -> Visualizar Jogador ");
-            System.Console.WriteLine(" 3 -> Visualizar Todos os Jogadores ");
-            System.Console.WriteLine(" 4 -> Modificar Jogador Especifico ");
-            System.Console.WriteLine(" 5 -> Remover Jogador Especifico ");
-            System.Console.WriteLine(" 6 -> Sair ");
-
+            System.Console.WriteLine(" 1 -> menu Jogador ");
+            System.Console.WriteLine(" 2 -> menu Inimigo ");
+            System.Console.WriteLine(" 3 -> Sair ");
+        
             System.Console.Write("Inserir index de navegação: ");
             menu = int.Parse(Console.ReadLine());
-           
-            switch (menu){
-                case 1:
-                    jogadorController.CadastrarJogador();
-                    break;
-                case 2:
-                    jogadorController.VerJogador();
-                    break;
-                case 3:
-                    jogadorController.VerTodosJogadores();
-                    break;
-                case 4:
-                    jogadorController.ModificarJogadorEspecifico();
-                    break;
-                case 5:
-                    jogadorController.RemoverJogadorEspecifico();
-                    break;
+        
+            while(menu == 1 && submenu != 6){
+                System.Console.WriteLine(" ============= Menu Jogador ============= ");
+                System.Console.WriteLine(" 1 -> Cadastrar Jogador ");
+                System.Console.WriteLine(" 2 -> Visualizar Jogador ");
+                System.Console.WriteLine(" 3 -> Visualizar Todos os Jogadores ");
+                System.Console.WriteLine(" 4 -> Modificar Jogador Especifico ");
+                System.Console.WriteLine(" 5 -> Remover Jogador Especifico ");
+                System.Console.WriteLine(" 6 -> Sair ");
+                
+                System.Console.Write("Inserir index de navegação: ");
+                submenu = int.Parse(Console.ReadLine());
+            
+                switch (submenu){
+                    case 1:
+                        jogadorController.CadastrarJogador();
+                        break;
+                    case 2:
+                        jogadorController.VerJogador();
+                        break;
+                    case 3:
+                        jogadorController.VerTodosJogadores();
+                        break;
+                    case 4:
+                        jogadorController.ModificarJogadorEspecifico();
+                        break;
+                    case 5:
+                        jogadorController.RemoverJogadorEspecifico();
+                        break;
+                }
+                
             }
+
+            while(menu == 2 && submenu !=6){
+                System.Console.WriteLine("submenu inimigo");
+                System.Console.WriteLine(" 1 -> Cadastrar inimigo ");
+                System.Console.WriteLine(" 2 -> Visualizar inimigo ");
+                System.Console.WriteLine(" 3 -> Visualizar Todos os inimigos ");
+                System.Console.Write("Inserir index de navegação: ");
+                submenu = int.Parse(Console.ReadLine());
+                switch (submenu){
+                    case 1:
+                        inimigoController.CadastrarInimigo();
+                        break;
+                    case 2:
+                        inimigoController.VerInimigo();
+                        break;
+                    case 3:
+                        inimigoController.VerTodosInimigos();
+                        break;
+                    case 4:
+                        inimigoController.ModificarInimigoEspecifico();
+                        break;
+                    case 5:
+                        inimigoController.RemoverInimigoEspecifico();
+                        break;
+
+            }
+            submenu = 0;
         }
         System.Console.WriteLine("Fim menu!");
     }
+}
 }
