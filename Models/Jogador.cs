c4c50d249314b8f3c906501d1fa65d31e4deb082
Hugo Victor <hugovic.it@gmail.com>
2023-05-29 05:49:00 -0300

    JsonIgnore e pesquisa na model Jogador

--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using rpg_console.Context;
 
 namespace rpg_console.Models;
 
 public class Jogador
 {
+ 
+    private readonly AppDbContext contexto = new AppDbContext{};
+
     public int JogadorId { get; set; }
     public string? Nome { get; set; }
     public int HpMax { get; set; }
@@ -18,17 +23,22 @@ public class Jogador
     public int Experiencia { get; set; }
     public int Moedas { get; set; }
     //public EquipamentoArma? Arma { get; set; } //remove ?
+    [JsonIgnore]
     public int EquipamentoArmaId { get; set; } //EF convencao
     public EquipamentoArmadura? Armadura { get; set; }
     public ICollection<Item>? Itens {get; set;}
 
     public override string ToString()
     {
+        
+        
         if(EquipamentoArmaId is 0){
             return $"Id:{JogadorId} Nome:{Nome} HpMax:{HpMax} MpMax:{MpMax} Forca:{Forca} Defesa:{Defesa} Experiencia:{Experiencia} Moedas:{Moedas}"; 
         }
         
-        return $"Id:{JogadorId} Nome:{Nome} HpMax:{HpMax} MpMax:{MpMax} Forca:{Forca} Defesa:{Defesa} Experiencia:{Experiencia} Moedas:{Moedas} ArmaId:{EquipamentoArmaId}"; 
+
+        var resultado = contexto.Armas.SingleOrDefault(x => x.EquipamentoArmaId == EquipamentoArmaId);
+        return $"Id:{JogadorId} Nome:{Nome} HpMax:{HpMax} MpMax:{MpMax} Forca:{Forca} Defesa:{Defesa} Experiencia:{Experiencia} Moedas:{Moedas} ArmaId:{EquipamentoArmaId} ArmaNome:{resultado} "; 
 
     }
 }
